b6a6fc068d2d9731e3aeadce400b4573ee2984af
Nidhushan <nidhushun06@gmail.com>
2023-12-31 13:01:21 -0500

    Portfolio skeleton

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -1,12 +1,27 @@
 // Navbar.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isScrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      setScrolled(offset > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="nav-links">
-        <img src="./components/logo.svg"></img>
+        <img src="./components/logo.svg"/>
         <a href="#profile">Profile</a>
         <a href="#projects">Projects</a>
         <a href="#blogs">Blogs</a>
